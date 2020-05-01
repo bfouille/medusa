3eb8d088b6dc2d864d2eb0f8c88bd1afdfde6c11
bfouille <58021830+bfouille@users.noreply.github.com>
2020-05-01 16:44:14 +0800

    Update postMedusa.py

--- extras/postMedusa.py
+++ extras/postMedusa.py
@@ -1,6 +1,10 @@
 import subprocess
 import sys
+import os                                                                                                                                  
+                                                                                                                                           
+SMA_PATH= os.environ['SMA_PATH']                                                                                                                                                                                                                                                   
+subprocess.call([SMA_PATH + '/venv/bin/python3', SMA_PATH + '/postSickbeard.py'] + sys.argv[1:]) 
 
-subprocess.call(['$SMA_PATH/venv/bin/python3', '$SMA_PATH/postSickbeard.py'] + sys.argv[1:])
+#subprocess.call(['$SMA_PATH/venv/bin/python3', '$SMA_PATH/postSickbeard.py'] + sys.argv[1:])
 # If you need to run more scripts (optional)
 # subprocess.call(['my_cmd2', 'my_script2'] + sys.argv[1:])
